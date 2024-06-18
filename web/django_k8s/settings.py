0bf3e8e9b0e8275c09c99eafa3049e25eb02205f
gouemoregis <gouemoregis@gmail.com>
2024-06-18 16:26:54 +0200

    Added posts model and test

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # external
     'storages',
+    # internal
+    'posts',
 ]
 
 MIDDLEWARE = [
